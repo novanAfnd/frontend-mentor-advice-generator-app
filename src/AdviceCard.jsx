d001ce3bd9e5b33323016a7029ecdee945f363e1
Novan Afandi <novanafandi01@gmail.com>
2024-06-28 08:05:01 +0700

    Setting CSS

---
+++ src/AdviceCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AdviceCard = () => {
+  return (
+    <div className="card">
+      <h1 className="card__header">ADVICE #</h1>
+      <p className="card__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium magni odio nostrum. Excepturi officia,
+        fugit asperiores maxime nisi odio quas omnis distinctio eius sint deserunt sed facilis ratione assumenda.
+        Exercitationem.
+      </p>
+      <img src="" alt="" className="card__divider" />
+      <button className="card__button"></button>
+    </div>
+  );
+};
+
+export default AdviceCard;
--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import AdviceCard from "./AdviceCard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <AdviceCard />
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a>sadfsdaf</a>
+        Coded by <a href="#">Novan Afandi</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
