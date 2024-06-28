151b8ff98c566d436cc47bbb54148164b4aa765b
Novan Afandi <novanafandi01@gmail.com>
2024-06-28 20:45:47 +0700

    Fix many thing
    Fixed the CSS so that the footer can be at the bottom, add image, learn react

--- src/AdviceCard.jsx
+++ src/AdviceCard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import patternDividerMobile from "./assets/images/pattern-divider-mobile.svg";
+import patternDividerDesktop from "./assets/images/pattern-divider-desktop.svg";
+import iconDice from "./assets/images/icon-dice.svg";
 
 const AdviceCard = () => {
   return (
     <div className="card">
       <h1 className="card__header">ADVICE #</h1>
-      <p className="card__text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium magni odio nostrum. Excepturi officia,
-        fugit asperiores maxime nisi odio quas omnis distinctio eius sint deserunt sed facilis ratione assumenda.
-        Exercitationem.
-      </p>
-      <img src="" alt="" className="card__divider" />
-      <button className="card__button"></button>
+      <p className="card__text">Lorem ipsum m.</p>
+      <div className="card__divider"></div>
+      <button className="card__button">
+        <img src={iconDice} alt="dice" />
+      </button>
     </div>
   );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import AdviceCard from "./AdviceCard";
 
 function App() {
   return (
-    <>
-      <AdviceCard />
-      <div className="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        Coded by <a href="#">Novan Afandi</a>
-      </div>
-    </>
+    <div className="body">
+      <main>
+        <AdviceCard />
+      </main>
+
+      <footer>
+        <div className="attribution">
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          Coded by <a href="#">Novan Afandi</a>
+        </div>
+      </footer>
+    </div>
   );
 }
 
