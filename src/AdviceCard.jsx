25280f756ffdf991969f7b769056af550dc2db98
Novan Afandi <novanafandi01@gmail.com>
2024-06-30 09:50:40 +0700

    Add Animation in button

--- src/AdviceCard.jsx
+++ src/AdviceCard.jsx
@@ -4,14 +4,26 @@ import patternDividerDesktop from "./assets/images/pattern-divider-desktop.svg";
 import iconDice from "./assets/images/icon-dice.svg";
 
 const AdviceCard = () => {
-  // State untuk membuat button dice berputar saat diklik
+  // Baris kode untuk membuat tombol berputar saat diklik
+  const [rotate, setRotate] = useState(false); // tetapkan dulu rotate = false
+
+  function handleClick() {
+    setRotate(true); // buat rotate menjadi true saat diklik
+
+    setTimeout(() => {
+      setRotate(false); // buat rotate menjadi false setelah 1 detik
+    }, 1000);
+  }
 
   return (
     <div className="card">
       <h1 className="card__header">ADVICE # {"117"}</h1>
       <p className="card__text">"{"..."}"</p>
       <div className="card__divider"></div>
-      <button className="card__button"></button>
+      <button
+        className={`card__button ${rotate === true ? "card__button-rotating" : ""}`}
+        onClick={handleClick}
+      ></button>
     </div>
   );
 };
